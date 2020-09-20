b92f3f4a56437b232645a0a0bbc4aeef2da5f145
Daniel Mladek <daniel_mladek@trimble.com>
2020-09-21 09:12:17 +1200

    removed unused imports

--- BookLibrary/src/test/java/net/delphym/booklib/BookLibraryApplicationTests.java
+++ BookLibrary/src/test/java/net/delphym/booklib/BookLibraryApplicationTests.java
@@ -1,11 +1,8 @@
 package net.delphym.booklib;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
